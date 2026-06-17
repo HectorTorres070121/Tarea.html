8dcc7c4c6951b5b0f7f27dde15620da4431f34c6
eloy torres <20253rd037@utez.edu.mx>
2026-06-17 10:41:23 -0600

    add

---
+++ css/js/app.js
@@ -0,0 +1,14 @@
+function mostrarAlertaInterna(){
+    alert("hola mundo desde archivo externo")
+}
+
+var nombreVariable='valor1';
+let nombreVariable2='valor2';
+console.log("hola mundo desde la consola")
+nombreVariable2= 2;
+
+const nombreconstante = true;
+nombreconstante = false;
+
+
+
